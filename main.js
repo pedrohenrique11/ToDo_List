583db94c5eb8b14624c3261d59c0ecac89cb1c1c
Pedro Henrique <pedroballack1167@gmail.com>
2023-02-24 15:06:25 -0300

    refatorando o codigo

--- main.js
+++ main.js
@@ -1,31 +1,81 @@
 const addTaskButton = document.getElementById("newTaskButton"); 
-const newTask = document.getElementById("newTask");
-const taskList = document.getElementById("taskList");
+const newTaskInput = document.getElementById("newTaskInput");
+const taskListContainer = document.getElementById("taskList");
 
-// Adicionar as novas tarefas
-addTaskButton.addEventListener("click", addNewTask)
 
+
+
+
+class TaskList {
+    constructor() {
+        this.task = []; // atribuindo o valor de task ao array
+    }
+    addTask(description) {
+        const task = new Tpasask(description) // criando novo objeto 
+        this.task.push(task)
+    }
+    removeTask(index) {
+        this.task.splice(index, 1) // removendo a task do array
+    }
+    getTaskList(){
+        return this.task
+    }
+}
+
+const taskList = new TaskList();
+
+addTaskButton.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const description = newTaskInput.value.trim();
+    if (description !== "") {
+        console.log("esta funcionando")
+        taskList.addTask(description);
+        taskListContainer.innerHTML = renderTask(taskList.getTaskList()); // taskList
+    }
+    else alert("You forgot to write your task!")
+})
+
+function renderTask(taskList) {
+    return taskList.map((task) => {
+        const doneClass = task.done ? "task done" : "task";
+        return `<li class="${doneClass}">
+                    <div>
+                        <input type="checkbox" class="checkbox">
+                        <p>${task.description}</p>
+                    </div>
+                    <button>Delete</button>
+                </li>
+    `;
+    }).join("");
+}
+
+/*
 function addNewTask(event) {
     event.preventDefault();
 
-        const task = newTask.value;
+        const task = newTaskInput.value;
 
         const li = document.createElement("li"); // Criando o intem da lisa
         li.classList.add('task');
         
         const input = document.createElement("input"); // criando botão de checklist 
-        input.classList.add("checkbox");
+        const taskCheck = input.checked == true ? input.classList.add("checkbox taskCheck") : input.classList.add("checkbox")
+        //input.classList.add("checkbox");
         input.type = "checkbox";
 
         const text = document.createElement("p"); // Criando o conteudo da tarefa
         text.innerHTML = `${task}`;
 
-        taskList.appendChild(li);
-        li.appendChild(input);
+        const remove = document.createElement("button"); // botão de remover tarefa
+        remove.classList.add("delete_button");
+        
+
+        taskList.appendChild(li)
+        li.appendChild(input)
         li.appendChild(text)
+        li.appendChild(remove)
 
-
-    
         console.log(task)
     } 
-    
+    */
